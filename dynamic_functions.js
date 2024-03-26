babcba7336090e16efceb82016fe251ac8eb4926
Bálint Biró <76958102+balintbiro@users.noreply.github.com>
2024-03-26 11:23:11 +0100

    New look & features
    Cleaner look plus additional features like navigation panel and sticky header.

--- dynamic_functions.js
+++ dynamic_functions.js
@@ -1,9 +1,21 @@
-/* Set the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidebar").style.width = "150px";
+    document.getElementById("main").style.marginLeft = "175px";
+    document.getElementById("openbtn").style.visibility="hidden";
+}
+  
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("openbtn").style.visibility="visible";
 }
 
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("title").innerHTML='MANUDB';
+  } else {
+    document.getElementById("title").innerHTML='MANUDB<br>MAmmalian NUclear mitochondrial sequences DataBase';
+  }
 }
