9d1396ce238725a43b8e5de3ee20327101441cc4
Bálint Biró <76958102+balintbiro@users.noreply.github.com>
2024-04-23 11:56:46 +0200

    Update dynamic_functions.js

--- dynamic_functions.js
+++ dynamic_functions.js
@@ -1,21 +1,50 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "150px";
-    document.getElementById("main").style.marginLeft = "175px";
-    document.getElementById("openbtn").style.visibility="hidden";
-}
-  
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.getElementById("openbtn").style.visibility="visible";
-}
+// functions for sticky header
+window.onscroll = function() {Stick()};
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("title").innerHTML='MANUDB';
+function Stick() {
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky")
   } else {
-    document.getElementById("title").innerHTML='MANUDB<br>MAmmalian NUclear mitochondrial sequences DataBase';
+    navbar.classList.remove("sticky");
   }
+}
+
+// functions for showing/clearing examples
+function showExample() {
+  if (document.getElementById("inner").value==""){
+    let element_list=['>Example_sequence','ACGGGGTACGCGATTTTTTCATCATCAGCTTGTTTTGTCGATCGTACGATCGCTTTAGGGGGGAACAAT']
+    for (let i=0, l=element_list.length; i<l; i++)
+      document.getElementById("inner").value += element_ist[i]+'\n'
+  }
+}
+
+function clearExample() {
+  document.getElementById("inner").value="";
+}
+
+// functions for plotting
+var samplePlot = document.getElementById('samplePlot');
+function drawLines() {
+  var one = {
+     x: [1,2,3,4,5,6,7,8,9,10,11],
+     y: [1,4,3,3,9,12,10,9,12,20,15],
+     mode: 'lines+markers',
+     name: 'Sample Data'
+  };
+  var data = [one];
+  var layout = {
+    title: {
+      text: 'Sample Plot'
+    },
+    xaxis: {
+      title: {text: 'X axis'}
+    },
+    yaxis: {
+      title: {text: 'Y axis'}
+    }
+  };
+  var config = {responsive: true}
+  Plotly.newPlot(samplePlot, data, layout, config);
 }
