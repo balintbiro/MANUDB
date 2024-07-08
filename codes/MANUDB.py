9adbd64070f5117c5bf3f0dc3afc2ff9cfaf2095
Balint Biro <biro.balint.szie@gmail.com>
2024-07-08 14:25:43 +0200

    check versions

--- codes/MANUDB.py
+++ codes/MANUDB.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+import sys
 import xgboost
 import numpy as np
 import pandas as pd
@@ -20,5 +21,5 @@ st.write("Lorem Ipsum"*100)
 X=pd.DataFrame(np.random.randint(0,100,size=(100,4)),columns=list('ABCD'))
 y=np.random.choice([0,1],100)
 
-clf=xgboost.XGBClassifier()
-clf.fit(X,y)
+st.write(xgboost.__version__)
+st.write(sys.version)
