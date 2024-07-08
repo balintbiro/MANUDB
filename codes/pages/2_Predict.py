2efa999ed5094a01825fc1ea866aeda3a8172ef3
Balint Biro <biro.balint.szie@gmail.com>
2024-07-08 10:51:37 +0200

    add versions

--- codes/pages/2_Predict.py
+++ codes/pages/2_Predict.py
@@ -1,10 +1,6 @@
-import joblib
 import xgboost
-import numpy as np
 import pandas as pd
 import streamlit as st
-from itertools import product
-from ast import literal_eval
 
 
 X=pd.DataFrame(np.random.randint(0,100,size=(100,4)),columns=list('ABCD'))
