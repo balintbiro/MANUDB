fb44150ba61f35c2011d3201d00a23766f61def5
Balint Biro <biro.balint.szie@gmail.com>
2024-07-08 10:27:31 +0200

    add xgboost

--- codes/pages/2_Predict.py
+++ codes/pages/2_Predict.py
@@ -1,35 +1,13 @@
 import pickle
+import xgboost
 import numpy as np
 import pandas as pd
 import streamlit as st
 from itertools import product
 st.set_page_config(page_title='Predict')
 
-
-import pickle
-import xgboost
-import pandas as pd
-from ast import literal_eval
-
-
+trained_clf=pickle.load(open('results/optimized_model.pkl','rb'))
 best_features=pd.read_csv('results/best_features.csv',index_col=0)['0'].tolist()
-best_params=literal_eval(
-    pd.read_csv('results/param_search_result.csv',index_col=0)
-    .sort_values(by='polygon_area',ascending=False)
-    .iloc[0]['params']
-)
-features=pd.read_csv('data/NUMT_features.csv')
-features=features[best_features+['label']]
-X,y=features.drop(columns=['label']),features['label'].replace(['random','numt'],[0,1]).values
-
-
-clf=xgboost.XGBClassifier(**best_params)
-trained_clf=clf.fit(X,y)
-
-
-
-'''trained_clf=pickle.load(open('results/optimized_model.pkl','rb'))
-best_features=pd.read_csv('results/best_features.csv',index_col=0)['0'].tolist()'''
 
 
 k=3
