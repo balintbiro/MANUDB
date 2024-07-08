239ef2b8f6ee46f2df43512f9170160e4f730b0d
Balint Biro <biro.balint.szie@gmail.com>
2024-07-08 10:40:43 +0200

    Update 2_Predict.py

--- codes/pages/2_Predict.py
+++ codes/pages/2_Predict.py
@@ -5,8 +5,17 @@ import pandas as pd
 import streamlit as st
 from itertools import product
 
-st.set_page_config(page_title='Predict')
 
-trained_clf=joblib.load(open('results/optimized_model.pkl','rb'))
+best_features=pd.read_csv('results/best_features.csv',index_col=0)['0'].tolist()
+best_params=literal_eval(
+    pd.read_csv('results/param_search_result.csv',index_col=0)
+    .sort_values(by='polygon_area',ascending=False)
+    .iloc[0]['params']
+)
+features=pd.read_csv('data/NUMT_features.csv')
+features=features[best_features+['label']]
+X,y=features.drop(columns=['label']),features['label'].replace(['random','numt'],[0,1]).values
 
-st.write(trained_clf.get_params())
+
+clf=xgboost.XGBClassifier(**best_params)
+optimized_model=clf.fit(X,y)
