43106cf310fcf97c3d35d0ee2ecadc8d6a6709cc
Balint Biro <biro.balint.szie@gmail.com>
2024-07-10 08:29:53 +0200

    remove references section
    Remove the references section which made the page a bit too crowded. Replace the in text citations with authors plus year.

--- MANUDB.py
+++ MANUDB.py
@@ -24,39 +24,38 @@ st.html(
 
 #general information about the site
 st.header("MANUDB")
-st.header(
-    '''
+st.markdown(
+    '''<div style="text-align: left; font-weight: 900;">
     What is MANUDB?
-    '''
+    </div>'''
 )
 st.markdown(
-    '''<div style="text-align: justify; font-weight: 900;">
+    '''<div style="text-align: justify;">
     There is an ongoing process in which mitochondrial sequences are
     being integrated into the nuclear genome. These sequences are called NUclear MiTochondrial sequences (NUMTs)
-    <a href="https://www.sciencedirect.com/science/article/pii/S0888754396901883?via%3Dihub">[1]</a>.<br>
+    <a href="https://www.sciencedirect.com/science/article/pii/S0888754396901883?via%3Dihub">(Lopez et al., 1996)</a>.<br>
     The importance of NUMTs has already been revealed in cancer biology
-    <a href="https://link.springer.com/article/10.1186/s13073-017-0420-6">[2]</a>,
-    forensic <a href="https://www.sciencedirect.com/science/article/pii/S1872497321000363">[3]</a>,
-    phylogenetic studies <a href="https://journals.plos.org/plosgenetics/article?id=10.1371/journal.pgen.1000834">[4]</a>
+    <a href="https://link.springer.com/article/10.1186/s13073-017-0420-6">(Srinivasainagendra et al., 2017)</a>,
+    forensic <a href="https://www.sciencedirect.com/science/article/pii/S1872497321000363">(Marshall & Parson, 2021)</a>,
+    phylogenetic studies <a href="https://journals.plos.org/plosgenetics/article?id=10.1371/journal.pgen.1000834">(Hazkani-Covo et al., 2010)</a>
     and in the evolution of the eukaryotic genetic information
-    <a href="https://www.sciencedirect.com/science/article/pii/S1055790317302609?via%3Dihub">[5]</a>.<br> 
+    <a href="https://www.sciencedirect.com/science/article/pii/S1055790317302609?via%3Dihub">(Nacer & Raposo do Amaral, 2017)</a>.<br> 
     Human and numerous model organisms’ genomes were described from the NUMTs point of view.
     Furthermore, recent studies were published on the patterns of these nuclear localised mitochondrial sequences
-    in different taxa <a href="https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0286620">[6
-    <a href="https://www.biorxiv.org/content/10.1101/2022.08.05.502930v2.full.pdf">,7].</a> <br>
+    in different taxa <a href="https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0286620">(Hebert et al., 2023; 
+    <a href="https://www.biorxiv.org/content/10.1101/2022.08.05.502930v2.full.pdf">,Biró et al., 2022).</a> <br>
     However, the results of the previously released studies are difficult to compare 
     due to the lack of standardised methods and/or using few numbers of genomes. To overcome this limitations,
     our group has already published a computational pipeline to mine NUMTs
-    <a href="https://bmcgenomics.biomedcentral.com/articles/10.1186/s12864-024-10201-9">[8]</a>. Therefore, our goal with MANUDB is to
+    <a href="https://bmcgenomics.biomedcentral.com/articles/10.1186/s12864-024-10201-9">(Biró et al., 2024)</a>. Therefore, our goal with MANUDB is to
     store, visualize, predict and make NUMTs accessible that were mined with our workflow.
     </div>''',
     unsafe_allow_html=True
 )
-#############################
-st.header(
-    '''
+st.markdown(
+    '''<div style="text-align: left; font-weight: 900;">
     Current status and functionalities
-    '''
+    </div>'''
 )
 st.markdown(
     '''<div style="text-align: justify;">
@@ -69,11 +68,10 @@ st.markdown(
     </div>''',
     unsafe_allow_html=True
 )
-#############################
-st.header(
-    '''
+st.markdown(
+    '''<div style="text-align: left; font-weight: 900;">
     Contact and bug report
-    '''
+    </div>'''
 )
 st.markdown(
     '''<div style="text-align: justify;">
@@ -86,11 +84,10 @@ st.markdown(
     </div>''',
     unsafe_allow_html=True
 )
-#############################
-st.header(
-    '''
+st.markdown(
+    '''<div style="text-align: left; font-weight: 900;">
     Upon usage please cite
-    '''
+    </div>'''
 )
 st.markdown(
     '''<div style="text-align: justify;">
@@ -99,8 +96,7 @@ st.markdown(
     </div>''',
     unsafe_allow_html=True
 )
-#############################
-
+#sidebar for navigation between chapters
 with st.sidebar:
     st.markdown("[MANUDB](#manudb)")
     st.markdown("[Export](#export)")
@@ -127,6 +123,7 @@ st.markdown(
 connection=sqlite3.connect('MANUDB.db')
 cursor=connection.cursor()
 
+#get the organism names by querying the DB
 organism_names=(
     pd.read_sql_query(
             """
@@ -150,6 +147,7 @@ organism_name=st.selectbox(
 def convert_df(df):
     return df.to_csv(index=False).encode('utf-8')
 
+#a dictionary for the SQL queries
 if organism_name!=None:
     queries={
         'Statistic':pd.read_sql_query(
@@ -339,31 +337,3 @@ if 'prediction' in st.session_state:
 	csv = convert_df(st.session_state['prediction'])
 	st.download_button(f"Download MANUD_prediction.csv",csv,f"MANUD_prediction.csv","text/csv",key='download-DBpart')
 	del st.session_state['prediction']
-#########################################################################
-st.divider()
-st.header(
-    '''
-    References
-    '''
-)
-st.markdown(
-    '''<div style="text-align: justify;">
-    [1] Lopez, J. V., Cevario, S., & O'Brien, S. J. (1996). Complete nucleotide sequences of the domestic cat (Felis catus)
-    mitochondrial genome and a transposed mtDNA tandem repeat (Numt) in the nuclear genome. Genomics, 33(2), 229-246.<br>
-    [2] Srinivasainagendra, V., Sandel, M. W., Singh, B., Sundaresan, A., Mooga, V. P., Bajpai, P., ... & Singh, K. K. (2017).
-    Migration of mitochondrial DNA in the nuclear genome of colorectal adenocarcinoma. Genome medicine, 9, 1-15.<br>
-    [3] Marshall, C., & Parson, W. (2021). Interpreting NUMTs in forensic genetics: Seeing the forest for the trees.
-    Forensic Science International: Genetics, 53, 102497.
-    [4] Hazkani-Covo, E., Zeller, R. M., & Martin, W. (2010). Molecular poltergeists: mitochondrial DNA copies (numts) in
-    sequenced nuclear genomes. PLoS genetics, 6(2), e1000834.<br>
-    [5] Nacer, D. F., & do Amaral, F. R. (2017). Striking pseudogenization in avian phylogenetics: Numts are large and
-    common in falcons. Molecular phylogenetics and evolution, 115, 1-6.<br>
-    [6] Hebert, P. D., Bock, D. G., & Prosser, S. W. (2023). Interrogating 1000 insect genomes for NUMTs: A risk
-    assessment for estimates of species richness. PLoS One, 18(6), e0286620.<br>
-    [7] Biró, B., Gál, Z., Brookman, M., & Hoffmann, O. I. (2022). Patterns of numtogenesis in sixteen different mice strains.
-    bioRxiv, 2022-08.<br>
-    [8] Biró, B., Gál, Z., Fekete, Z., Klecska, E., & Hoffmann, O. I. (2024). Mitochondrial genome plasticity of
-    mammalian species. BMC genomics, 25(1), 278.
-    </div>''',
-    unsafe_allow_html=True
-)
