4fe76f2712b30302652f225e3e329d82cd8f463a
Bálint Biró <biro.balint@uni-mate.hu>
2024-10-30 11:46:14 +0900

    Get annotations
    Mine annotations based on GBFF files and merge them. Ready to be loaded into the SQL DB.

--- prep_files/getMtAnnotations.py
+++ prep_files/getMtAnnotations.py
@@ -1,9 +1,11 @@
 #importations
+import sqlite3
 import numpy as np
 import pandas as pd
 from Bio import SeqIO
 
 MtIDs=pd.read_csv("../scratch/MANUDB_MtIDs.csv")
+con=sqlite3.connect("../scratch/MANUDB.db")
 
 orgnames=MtIDs["orgname"].tolist()
 
@@ -20,5 +22,31 @@ for record in SeqIO.parse("../scratch/mitochondrion.1.genomic.gbff","genbank"):
 					else:
 						df.append([orgname,feature.type,feature.location.start,feature.location.end,feature.qualifiers.get("gene",np.nan)])
 
-df=pd.DataFrame(data=df,columns=["orgname","type","start","end","gene"])
-df.to_csv("../scratch/MtAnnotations.csv",index=False)
+MtAnnotations=pd.DataFrame(data=df,columns=["orgname","type","start","end","gene"])
+MtAnnotations.to_csv("../scratch/MtAnnotations.csv",index=False)
+
+NUMTs=pd.read_sql_query("SELECT * FROM location",con=con)
+NUMTs["orgname"]=NUMTs["id"].str.split("_").str[:2].str.join(" ")
+MtAnnotations=MtAnnotations[MtAnnotations["type"].isin(["gene","tRNA"])]
+
+def MtIntragenic(row):
+	orgname=row["orgname"]
+	subdf=MtAnnotations[MtAnnotations["orgname"]==orgname]
+	Nstart,Nend=row["mitochondrial_start"],row["mitochondrial_start"]+row["mitochondrial_length"]
+	subdf=subdf[
+		((Nstart<subdf["start"])&(Nend>subdf["start"]))
+		|((Nstart>subdf["start"])&(Nend<subdf["end"]))
+		|((Nstart<subdf["end"])&(Nend>subdf["end"]))
+		|((Nstart<subdf["start"])&(Nend>subdf["end"]))
+	]
+	if subdf.shape[0]==0:
+		return np.nan
+	elif subdf.shape[0]==1:
+		return subdf["gene"].values[0]
+	else:
+		return ",".join(subdf["gene"].tolist())
+
+NUMTs["mt_gene"]=NUMTs.apply(MtIntragenic,axis=1)
+
+#merge it with intragenic NUMTs
+NUMTs[["id","mt_gene"]].join(pd.read_csv("../scratch/IntragenicNUMTs.tsv",sep="\t").set_index("id"),on="id").dropna(subset=["mt_gene","gene"],how="all").to_csv("../scratch/allGenes.tsv",sep="\t",index=False)
