65f1f4d28d1619c3dbf729ef8377e777fb4d26a2
Balint Biro <biro.balint.szie@gmail.com>
2025-01-03 09:27:13 +0100

    New schema
    New schema mods:
    - NUMT level info in one table (general_info)
    - genes (g and mt) are in separate tables for the sake of storage efficiency
    - same for taxonomy

--- prep_files/database_creation.py
+++ prep_files/database_creation.py
@@ -2,30 +2,30 @@
 import sqlite3
 import pandas as pd
 
-#read in the data
-numts=pd.read_csv('../../../../../Downloads/numts.csv',index_col=0)
-assemblies=pd.read_csv('../data/assembly_versions.csv',index_col=0)
+#read in the data from the previous version of DB
+connection=sqlite3.connect("MANUDBrev.db")
+stat=pd.read_sql_query("SELECT * FROM statistic",connection)
+loc=pd.read_sql_query("SELECT * FROM location",connection)
+gen=pd.read_sql_query("SELECT * FROM genes",connection)
+tax=pd.read_sql_query("SELECT * FROM taxonomy",connection)
 
-#add ID column for further joins
-numts['id']=numts['organism_name']+'_'+numts.index.astype('str')
+#clean the genes table-> dropnas
+mtgen=gen[["id","mt_gene"]].dropna(subset="mt_gene")
+ugen=gen[["id","gene"]].dropna(subset="gene")
+
+#join the statistic and location tables as they are on the NUMTs level (+sequences)
+statloc=loc.join(stat.set_index("id"),on="id")
+
+#adding organism name. This will be the field for joining with the taxonomy table
+statloc["organism_name"]=statloc["id"].str.split("_").str[:-1].str.join('_')
 
 #connect to DB and initialize cursor
-connection=sqlite3.connect('../data/MANUDB.db')
-cursor=connection.cursor()
+newconnection=sqlite3.connect('MANUDBrev2.db')
+cursor=newconnection.cursor()
 
-#add tables
-
+#create table
 cursor.execute(
-        """CREATE TABLE statistic(
-            id VARCHAR,
-            eg2_value FLOAT,
-            e_value FLOAT,
-            seq_identity FLOAT,
-            alignment_score INT
-        )"""
-    )
-cursor.execute(
-        """CREATE TABLE location(
+        """CREATE TABLE general_info(
             id VARCHAR,
             genomic_id VARCHAR,
             genomic_size INT,
@@ -34,16 +34,15 @@ cursor.execute(
             genomic_length INT,
             mitochondrial_length INT,
             genomic_strand CHAR(1),
-            mitochondrial_strand CHAR(1)
+            mitochondrial_strand CHAR(1),
+            eg2_value FLOAT,
+            e_value FLOAT,
+            seq_identity FLOAT,
+            alignment_score INT,
+            organism_name VARCHAR
         )"""
     )
-cursor.execute(
-        """CREATE TABLE genes(
-            id VARCHAR,
-            gene VARCHAR,
-            mt_gene VARCHAR
-        )"""
-    )
+#create table
 cursor.execute(
         """CREATE TABLE taxonomy(
             organism_name VARCHAR,
@@ -53,6 +52,23 @@ cursor.execute(
             assembly_version VARCHAR
         )"""
     )
+
+#create table
+cursor.execute(
+        """CREATE TABLE mt_gene(
+            id VARCHAR,
+            mt_gene VARCHAR
+        )"""
+    )
+
+#create table
+cursor.execute(
+        """CREATE TABLE gene(
+            id VARCHAR,
+            gene VARCHAR
+        )"""
+    )
+
 
 #sanity check->should list all the table names
 result=cursor.execute("SELECT name FROM sqlite_master")
@@ -64,28 +80,48 @@ def fillDB(cols_list:list,query:str, input_df:pd.DataFrame)->None:
     for index,row in input_df[cols_list].iterrows():
         tuples_list.append(tuple(row.values))
     cursor.executemany(query, tuples_list)
-    connection.commit()
+    newconnection.commit()
 
 #fillDB with elements
 fillDB(
-    cols_list=['id','eg2_value','e_value','seq_identity','alignment_score'],
-    query="INSERT INTO statistic VALUES(?, ?, ?, ?, ?)",
-    input_df=numts
+    cols_list=[
+        'id','genomic_id','genomic_size','genomic_start','mitochondrial_start',
+        'genomic_length','mitochondrial_length','genomic_strand','mitochondrial_strand',
+        'eg2_value','e_value','seq_identity','alignment_score','organism_name'
+    ],
+    query="INSERT INTO general_info VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+    input_df=statloc
 )
+#fillDB with elements
 fillDB(
-    cols_list=['id','genomic_id','genomic_size','genomic_start','mitochondrial_start','genomic_length','mitochondrial_length','genomic_strand','mitochondrial_strand'],
-    query="INSERT INTO location VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)",
-    input_df=numts
+    cols_list=[
+        'organism_name','taxonomy_order','genus','family','assembly_version'
+    ],
+    query="INSERT INTO taxonomy VALUES(?,?,?,?,?)",
+    input_df=tax
 )
+#fillDB with elements
 fillDB(
-    cols_list=['id','genomic_sequence','mitochondrial_sequence'],
-    query="INSERT INTO sequence VALUES(?, ?, ?)",
-    input_df=numts
+    cols_list=[
+        'id','mt_gene'
+    ],
+    query="INSERT INTO mt_gene VALUES(?,?)",
+    input_df=mtgen
 )
+#fillDB with elements
 fillDB(
-    cols_list=['organism_name','taxonomy_order','family','genus','assembly_version'],
-    query="INSERT INTO taxonomy VALUES(?, ?, ?, ?, ?)",
-    input_df=assemblies
+    cols_list=[
+        'id','gene'
+    ],
+    query="INSERT INTO gene VALUES(?,?)",
+    input_df=ugen
 )
 
-pd.read_sql_query("""SELECT * FROM taxonomy where organism_name LIKE '%mus_musculus%'""",connection)
+#sanity checks
+pd.read_sql_query("""SELECT * FROM general_info where organism_name LIKE '%mus_musculus%'""",newconnection)
+pd.read_sql_query("""SELECT * FROM taxonomy where organism_name LIKE '%mus_musculus%'""",newconnection)
+pd.read_sql_query("""SELECT * FROM gene where id LIKE '%mus_musculus%'""",newconnection)
+pd.read_sql_query("""SELECT * FROM mt_gene where id LIKE '%mus_musculus%'""",newconnection)
+
+#closing the DB
+newconnection.close()
